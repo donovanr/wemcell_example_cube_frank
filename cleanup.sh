ea89031e5b171c3998f74f9d7882c4fd14158ea3
Rory Donovan <donovanr@alumni.reed.edu>
2015-06-16 15:03:21 -0400

    added cleanup script

---
+++ cleanup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+source variables.sh
+
+rm -rf $MODEL_NAME
+rm -rf istates 
+rm -rf seg_logs/
+rm -rf traj_segs/
+
+rm -f system.pyc
+rm -f variables.sh
+rm -f west.h5
+rm -f west.log
+
+rm -f variables.sh
+rm -f bstates/variables.sh
+
